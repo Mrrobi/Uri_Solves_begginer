74961bf1518561de0b604412aa815c218b83bc03
Md Robiuddin Robi <mrrobi040@gmail.com>
2017-10-06 23:35:38 +0600

    added

---
+++ 1001.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int A,B,X;
+    scanf("\n%d",&A);
+    scanf("\n%d",&B);
+    X=A+B;
+    printf("X = %d\n",X);
+    return 0;
+}
---
+++ 1002.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+#define PI 3.14159
+
+int main() {
+
+   double A,R;
+   scanf("%lf",&R);
+   A=(PI*pow(R,2));
+   printf("A=%.4lf\n",A);
+   return 0;
+}
